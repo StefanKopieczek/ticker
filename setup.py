e24f38f056d90b8cd7afb2550fc69ab1303df4eb
Stefan Kopieczek <stefankopieczek@gmail.com>
2014-10-16 21:48:52 +0100

    Add email to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='ticker',
       description='Yahoo finance wrapper for stock information',
       url='http://github.com/stefankopieczek/ticker',
       author='Stefan Kopieczek',
+      author_email="stefankopieczek+ticker@gmail.com",
       license='LGPL 2.0',
       packages=['ticker'],
       zip_safe=False)
