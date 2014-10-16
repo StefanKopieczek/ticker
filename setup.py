2749d62627b652aead1cbcaa3d1695d047230102
Stefan Kopieczek <stefankopieczek@gmail.com>
2014-10-16 21:59:17 +0100

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,8 @@ setup(name='ticker',
       author_email="stefankopieczek+ticker@gmail.com",
       license='LGPL 2.0',
       packages=['ticker'],
+      install_requires=[
+          'bs4',
+          'requests',
+      ],
       zip_safe=False)
